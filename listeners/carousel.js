650f9cb71a9e15fec65a1ff9ba5c5a9554792540
GuISatierf <guifreitas343@gmail.com>
2023-09-30 15:31:31 -0300

    Co-authored-by: Magalhaesz <Magalhaesz@users.noreply.github.com> Co-authored-by: Haniel Fialho <HanielCota@users.noreply.github.com>

--- listeners/carousel.js
+++ listeners/carousel.js
@@ -1,27 +1,34 @@
 const slider = document.querySelector('.carouselItems');
+const SCROLL_SPEED = 3;
+
 let isDown = false;
 let startX;
 let scrollLeft;
 
-slider.addEventListener('mousedown', e => {
+slider.addEventListener('mousedown', (e) => {
   isDown = true;
   slider.classList.add('active');
   startX = e.pageX - slider.offsetLeft;
   scrollLeft = slider.scrollLeft;
 });
-slider.addEventListener('mouseleave', _ => {
-  isDown = false;
-  slider.classList.remove('active');
-});
-slider.addEventListener('mouseup', _ => {
-  isDown = false;
-  slider.classList.remove('active');
-});
-slider.addEventListener('mousemove', e => {
+
+slider.addEventListener('mousemove', (e) => {
   if (!isDown) return;
   e.preventDefault();
   const x = e.pageX - slider.offsetLeft;
-  const SCROLL_SPEED = 3;
   const walk = (x - startX) * SCROLL_SPEED;
   slider.scrollLeft = scrollLeft - walk;
 });
+
+slider.addEventListener('mouseup', () => {
+  finishDrag();
+});
+
+slider.addEventListener('mouseleave', () => {
+  finishDrag();
+});
+
+function finishDrag() {
+  isDown = false;
+  slider.classList.remove('active');
+}
